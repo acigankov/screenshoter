651308c761ecd48c3ebf04ad8927866415ad7619
acigankov@inbox.ru <diiPs56uaY>
2022-11-24 15:40:42 +0300

    Добавил отслеживание клика вне тоглера языка, чтобы он закрывался

--- src/assets/js/language-toggler.js
+++ src/assets/js/language-toggler.js
@@ -2,8 +2,14 @@ const toggler = document.querySelector('.language-toggler');
 const togglerText = document.querySelector('.language-toggler span');
 const togglerItems = document.querySelectorAll('.language-dropdown__item');
 
-toggler.addEventListener('click', function (event) {
-   toggler.classList.toggle('language-toggler_open');
+//клик в окне
+window.addEventListener('click', function (event) {
+   if(event.target.closest('.language-toggler')) {
+      toggler.classList.toggle('language-toggler_open');
+      return false;
+   }
+   toggler.classList.remove('language-toggler_open');
+
 });
 
 togglerItems.forEach(function (elem){
